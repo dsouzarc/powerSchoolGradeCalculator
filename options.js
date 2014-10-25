95c15f8c3c893b795d74ac50c84df2e55e278b6c
Ryan D'souza <dsouzarc@gmail.com>
2014-10-25 17:18:56 -0400

    Saving goes through all input types

--- options.js
+++ options.js
@@ -1,16 +1,17 @@
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function save_options() {
-}
-
-function restore_options() {
-}
+document.addEventListener('DOMContentLoaded', restore_options);
 
 var counter = 1;
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+}
 
 function add_class() { 
 	if(counter <= 8) { 
 		const classNumber = counter;
+		var numWeights = 0;
+		
 		var header = document.createElement("h1");
 		header.setAttribute("name", "header" + classNumber);
 		header.appendChild(document.createTextNode("Period " + classNumber));		
@@ -26,9 +27,12 @@ function add_class() {
 		addWeightButton.id="add_weight" + classNumber;
 		document.getElementById("class_div").appendChild(addWeightButton);
 		
-		var numWeights = 0;
 	
 		document.getElementById("add_weight" + classNumber).onclick = function() { 
+			if(numWeights >= 5) { 
+				return;
+			}
+
 			var classificationLabel = document.createElement("p");
 			classificationLabel.appendChild(document.createTextNode("Assignment Type"));
 
@@ -57,7 +61,31 @@ function add_class() {
 	}
 }	
 
-document.addEventListener('DOMContentLoaded', restore_options);
+
+function save_options() {
+	var allElements = document.getElementsByTagName("div");
+	var allIds = [];
+	for (var i = 0, n = allElements.length; i < n; ++i) {
+  		var el = allElements[i];
+  		if (el.id) { 
+			allIds.push(el.id); 
+			var searchStuff = document.getElementById(allElements[i].id).children;
+			for(i = 0; i < searchStuff.length; i++) { 
+				allIds.push(searchStuff[i].value);
+			}
+		}
+	}
+alert("HERE" + allIds);
+var text = "";
+var elements = document.getElementsByTagName('input')
+  for(var i=0; i<elements.length; i++) {
+    var input = elements[i];
+    text += input.value + " VALUE";
+    //alert(input.value + " VALUE");
+  }
+alert(text);
+}
+
 document.getElementById('save').addEventListener('click',
     save_options);
 
