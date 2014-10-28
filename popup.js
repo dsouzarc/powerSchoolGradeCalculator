6532b819e703ef0548296129269b47ff1689e880
Ryan D'souza <dsouzarc@gmail.com>
2014-10-28 19:56:09 -0400

    Successfully shows all periods as popup

--- popup.js
+++ popup.js
@@ -6,6 +6,16 @@ function show_periods() {
     chrome.storage.sync.get(null, function(items) { 
         const allWeights = items.allWeights;
         const allIDs = items.allIDs;
-        alert(allWeights);
+
+        for(var i = 0; i < allWeights.length; i++) { 
+            var period = document.createElement("input");
+            period.setAttribute("type", "button");
+            period.setAttribute("value", "Period " + (i + 1));
+            document.getElementById("period_choices").appendChild(period);
+
+        }
     });
 }
+
+document.getElementById('do-count').onclick = new function() { 
+};
